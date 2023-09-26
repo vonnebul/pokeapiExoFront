37efc91d7d1a8bd6b9b243ee5f3e44ec926e68c6
Vonnebul <vonnebul@gmail.com>
2023-09-26 08:55:08 +0200

    exo

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import MesPokemons from './Components/MesPokemons';
+import MonPokedex from './Components/MonPokedex';
+import Navigation from './Components/Navigation';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+    <Navigation />
+    <Routes>
+      <Route path='/'/>
+      <Route path='/h' element={<MesPokemons/>}/>
+      <Route path='/MonPokedex' element={<MonPokedex/>}/>
+    </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/TypePokemon.js
@@ -0,0 +1,11 @@
+import "../style/TypePoke.css"
+
+function TypePokemon(props) {
+    let types = props.types
+    return (
+      <>
+      </>
+    );
+  }
+  
+  export default TypePokemon;
